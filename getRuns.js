c46018d1bc8af9fe8045a3d14f6c42540aa5430f
Thomas <66533806+Tchongas@users.noreply.github.com>
2023-10-12 14:53:57 -0300

    finished country rankings

--- getRuns.js
+++ getRuns.js
@@ -1,6 +1,12 @@
 let sliceValue1 = 0;
 let sliceValue2 = 3;
 
+document.getElementById("brBtn").addEventListener("click", function(){
+  document.getElementById("estado").innerHTML = "Brasil"
+});
+
+
+
 // Function to parse and create elements
 function generateDivWithClasses(container, classes) {
   let div = document.createElement('div');
@@ -21,7 +27,7 @@ function generateDivWithClasses(container, classes) {
 function generateDivWithValues(container, values, rowIndex) {
   let div = document.createElement('div');
   div.classList.add('d-flex', 'justify-content-between', 'runner');
-  
+
   let pIndex = document.createElement('p');
   pIndex.textContent = values[0]; // Display the row index
 
@@ -29,7 +35,13 @@ function generateDivWithValues(container, values, rowIndex) {
   p1.textContent = values[1]; // Use the first value from the array
   
   let p2 = document.createElement('p');
-  p2.textContent = values[2]; // Use the second value from the array
+  p2.textContent = values[2];
+  p2.href = 'https://example.com'; // Replace with your desired URL
+
+  let iconElement = document.createElement('i');
+  iconElement.classList.add('fa-solid', 'fa-arrow-up-right-from-square');
+
+  document.body.appendChild(p2);
   
   div.appendChild(pIndex);
   div.appendChild(p1);
@@ -81,8 +93,8 @@ function clearContainer(container) {
 
 // Add event listeners for different buttons to change slice values
 document.getElementById("ssgBtn").addEventListener("click", function() {
-  sliceValue1 = 12;
-  sliceValue2 = 15;
+  sliceValue1 = 13;
+  sliceValue2 = 17;
   document.getElementById("verDiv").style = "visibility: hidden;"
   parseAndCreateElements();
 });
@@ -100,15 +112,15 @@ document.getElementById("outrasBtn").addEventListener("click", function() {
 });
 
 document.getElementById("1.7Btn").addEventListener("click", function() {
-  sliceValue1 = 8;
-  sliceValue2 = 11;
+  sliceValue1 = 9;
+  sliceValue2 = 12;
 
   parseAndCreateElements();
   
 });
 
 document.getElementById("1.14Btn").addEventListener("click", function() {
-  sliceValue1 = 4;
+  sliceValue1 = 5;
   sliceValue2 = 8;
 
   parseAndCreateElements();
