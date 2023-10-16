c3a0a22b2ea3f5d8906a3ec2a5243c424b25456f
Thomas <66533806+Tchongas@users.noreply.github.com>
2023-10-16 20:50:00 -0300

    added all states

--- getStates.js
+++ getStates.js
@@ -68,550 +68,550 @@ function getState() {
   if (selectedState == "Paraná") {
     if (selectedCat == "1.16rsg") {
       sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue2 = 53;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 54;
+      sliceValue2 = 57;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 58;
+      sliceValue2 = 61;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 62;
+      sliceValue2 = 65;
       parseAndCreateElements();
     }
   }
   if (selectedState == "São Paulo") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 66;
+      sliceValue2 = 69;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 70;
+      sliceValue2 = 73;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 74;
+      sliceValue2 = 77;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 78;
+      sliceValue2 = 81;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Mato Grosso do Sul") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Rio de Janeiro") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 98;
+      sliceValue2 = 101;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 102;
+      sliceValue2 = 105;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 106;
+      sliceValue2 = 109;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 110;
+      sliceValue2 = 113;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Minas Gerais") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 114;
+      sliceValue2 = 117;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 118;
+      sliceValue2 = 121;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 122;
+      sliceValue2 = 125;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 126;
+      sliceValue2 = 129;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Goiás") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 130;
+      sliceValue2 = 133;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 134;
+      sliceValue2 = 137;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 138;
+      sliceValue2 = 141;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 142;
+      sliceValue2 = 145;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Distrito Federal") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 146;
+      sliceValue2 = 149;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 150;
+      sliceValue2 = 153;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 154;
+      sliceValue2 = 157;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 158;
+      sliceValue2 = 161;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Mato Grosso") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Espírito Santo") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Bahia") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 162;
+      sliceValue2 = 165;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 166;
+      sliceValue2 = 169;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 170;
+      sliceValue2 = 173;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 174;
+      sliceValue2 = 177;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Tocantins") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Pará") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 178;
+      sliceValue2 = 181;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 182;
+      sliceValue2 = 185;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 186;
+      sliceValue2 = 189;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 190;
+      sliceValue2 = 193;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Amazonas") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Acre") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Rondônia") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Sergipe") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Alagoas") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 194;
+      sliceValue2 = 197;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 198;
+      sliceValue2 = 201;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 202;
+      sliceValue2 = 205;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 206;
+      sliceValue2 = 209;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Pernambuco") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 210;
+      sliceValue2 = 213;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 214;
+      sliceValue2 = 217;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 218;
+      sliceValue2 = 221;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 222;
+      sliceValue2 = 225;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Paraíba") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 226;
+      sliceValue2 = 229;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 230;
+      sliceValue2 = 233;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 234;
+      sliceValue2 = 237;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 238;
+      sliceValue2 = 241;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Rio Grande do Norte") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 242;
+      sliceValue2 = 245;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 246;
+      sliceValue2 = 249;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 250;
+      sliceValue2 = 253;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 254;
+      sliceValue2 = 257;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Ceará") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Piauí") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Maranhão") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Amapá") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
   if (selectedState == "Roraima") {
     if (selectedCat == "1.16rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 82;
+      sliceValue2 = 85;
       parseAndCreateElements();
     }
     if (selectedCat == "1.16ssg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 86;
+      sliceValue2 = 89;
       parseAndCreateElements();
     }
     if (selectedCat == "1.14rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 90;
+      sliceValue2 = 93;
       parseAndCreateElements();
     }
     if (selectedCat == "1.7rsg") {
-      sliceValue1 = 50;
-      sliceValue2 = 50;
+      sliceValue1 = 94;
+      sliceValue2 = 97;
       parseAndCreateElements();
     }
   }
