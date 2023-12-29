8eb2108801f6e33e4bd344e35b167ae37b337684
Tchongas <66533806+Tchongas@users.noreply.github.com>
2023-12-29 01:16:34 -0300

    added index

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('brRankingButton').addEventListener('click', () => {
+    location.href = "https://docs.google.com/spreadsheets/d/1ovMyHrmk0J4BumPygSigTScmt7T-saKqaHrPCjSQG-Y/edit?usp=sharing";
+});
+
+document.getElementById('estadosRankingButton').addEventListener('click', () => {
+    location.href = "StateRanking/index.html";
+});
+
+document.getElementById('rankedRankingButton').addEventListener('click', () => {
+    location.href = "https://brazilian-mcsr-ranked-leaderboard.vercel.app/leaderboard";
+});
