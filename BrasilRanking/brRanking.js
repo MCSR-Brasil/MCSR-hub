300e671d2670c55c195debbf33d78db138ab72ad
Thomas Bocalon <tchongass1@gmail.com>
2025-04-24 18:05:45 -0300

    anchor on other files

--- BrasilRanking/brRanking.js
+++ BrasilRanking/brRanking.js
@@ -236,6 +236,25 @@ async function fetchBrRankingData(variableName, catStart, catEnd) {
         }
     }
    
-    fetchDataOnly('Principais', 0, 3);
-    fetch116SheetsBackend('116rsg', 11, 13);
+document.querySelectorAll('a').forEach(link => {
+    // Prefetch on hover
+    link.addEventListener('mouseenter', () => {
+      if (!link.dataset.prefetched) {
+        const prefetch = document.createElement('link');
+            prefetch.rel = 'prefetch';
+            prefetch.href = link.href;
+            document.head.appendChild(prefetch);
+            link.dataset.prefetched = 'true';
+          }
+        });
+    // Navigate quickly on mousedown
+    link.addEventListener('mousedown', e => {
+        if (e.button === 0 && !e.ctrlKey && !e.metaKey && !e.shiftKey) {
+            window.location.href = link.href;
+        }
+    });
+});
+
+fetchDataOnly('Principais', 0, 3);
+fetch116SheetsBackend('116rsg', 11, 13);
     
--- StateRanking/Scripts/index.js
+++ StateRanking/Scripts/index.js
@@ -94,3 +94,23 @@ am4core.ready(function () {
     chart.zoomControl = zoomControl;
     zoomControl.slider.height = 150;
   });
+
+document.querySelectorAll('a').forEach(link => {
+    // Prefetch on hover
+    link.addEventListener('mouseenter', () => {
+      if (!link.dataset.prefetched) {
+        const prefetch = document.createElement('link');
+        prefetch.rel = 'prefetch';
+        prefetch.href = link.href;
+        document.head.appendChild(prefetch);
+        link.dataset.prefetched = 'true';
+      }
+    });
+  
+    // Navigate quickly on mousedown
+    link.addEventListener('mousedown', e => {
+      if (e.button === 0 && !e.ctrlKey && !e.metaKey && !e.shiftKey) {
+        window.location.href = link.href;
+      }
+    });
+});
